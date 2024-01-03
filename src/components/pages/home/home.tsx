7e94d01413c7d0ca2cf3d17ea2afb4a8aa05cb70
Cristina Jung <cristijung@gmail.com>
2024-01-03 16:57:01 -0300

    home quase pronta

--- src/components/pages/home/home.tsx
+++ src/components/pages/home/home.tsx
@@ -1,12 +1,21 @@
 import styles from "./Home.module.scss";
+import imagem from '../../../assets/tela-home.png';
+
+
 
 export default function Home() {
   return (
     <>
-    <section className="content">      
-        <div className={styles.criative}>CRIATIVE</div>
+    <section className="content">
+      <div className={styles.container}>
+        <div className={styles.textContainer}>      
+        <div className={styles.criative}>CRIATIVE</div>        
         <div className={styles.interior}>INTERIOR</div>
-      
+        </div>
+        <div className={styles.imgContainer}> 
+          <img src={imagem} alt='imagem'/>         
+        </div>
+      </div>
     </section>
     </>
   );
